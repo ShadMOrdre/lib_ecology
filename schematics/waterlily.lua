15e829686315c2e52e05946834f68aacad28a41e
Shad MOrdre <shadmordre@gmail.com>
2018-01-19 12:38:21 -0800

    Add files via upload

---
+++ schematics/waterlily.lua
@@ -0,0 +1,20 @@
+
+-- waterlily
+
+lib_ecology.waterlily = {
+	size = {x = 1, y = 3, z = 1},
+	data = {
+
+		{name = "default:sand", param1 = 255},
+		{name = "default:water_source", param1 = 255},
+		{name = "flowers:waterlily", param1 = 255},
+
+	},
+}
+
+if lib_ecology.lilywalk == true then
+
+	--minetest.override_item("flowers:waterlily", {
+		walkable = true
+	--})
+end
