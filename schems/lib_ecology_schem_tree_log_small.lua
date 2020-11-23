5759315de6091668b6dc870e7b1bd9a428bf1a82
Shad MOrdre <shadmordre@gmail.com>
2020-11-23 08:57:43 -0800

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_log_small.lua
@@ -0,0 +1,15 @@
+
+
+
+
+--Log Small
+	local N = lib_ecology.N
+
+	
+	return {
+		size = {x = 3, y = 2, z = 1},
+		data = {
+			N["_____"], N["_____"], N["_____"],
+			N["T0mE1"], N["T0mE0"], N["T0mE2"]
+		},
+	}
