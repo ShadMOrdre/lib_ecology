57dde56267174e13a2a5b4240199fce3e00b98a0
Shad MOrdre <shadmordre@gmail.com>
2020-11-23 09:00:08 -0800

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_scorched_01.lua
@@ -0,0 +1,34 @@
+
+
+
+--Scorched 01
+	local N = lib_ecology.N
+
+	
+	return 	{
+		size = {x = 1,y = 10,z = 1},
+		data = {
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"]
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 254},
+			{ypos = 2,prob = 254},
+			{ypos = 3,prob = 254},
+			{ypos = 4,prob = 254},
+			{ypos = 5,prob = 254},
+			{ypos = 6,prob = 254},
+			{ypos = 7,prob = 254},
+			{ypos = 8,prob = 254},
+			{ypos = 9,prob = 254}
+		}
+	}
