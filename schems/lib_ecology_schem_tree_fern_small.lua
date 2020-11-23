b1214452aae937f11423847ccdc009ad93a067e4
Shad MOrdre <shadmordre@gmail.com>
2020-11-23 08:56:21 -0800

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_fern_small.lua
@@ -0,0 +1,57 @@
+
+	le_temp_schem = {
+		size = {x = 1,y = 4,z = 1},
+		data = {
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_leaves_01",prob = 254}
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 127},
+			{ypos = 2,prob = 254},
+			{ypos = 3,prob = 254}
+		}
+	}
+lib_ecology.schematics.insert("schem_tree_fern_small_01", le_temp_schem)
+
+	le_temp_schem = {
+		size = {x = 1,y = 4,z = 1},
+		data = {
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_leaves_02",prob = 254}
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 127},
+			{ypos = 2,prob = 254},
+			{ypos = 3,prob = 254}
+		}
+	}
+lib_ecology.schematics.insert("schem_tree_fern_small_02", le_temp_schem)
+
+	le_temp_schem = {
+		size = {x = 1,y = 6,z = 1},
+		data = {
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk_top",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_leaves",prob = 254}
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 127},
+			{ypos = 2,prob = 254},
+			{ypos = 3,prob = 127},
+			{ypos = 4,prob = 254},
+			{ypos = 5,prob = 254},
+			{ypos = 6,prob = 254}
+		}
+	}
+lib_ecology.schematics.insert("schem_tree_fern_small", le_temp_schem)
+
---
+++ schems/lib_ecology_schem_tree_fern_small_02.lua
@@ -0,0 +1,23 @@
+
+
+
+
+--Samll Fern Tree 02
+	local N = lib_ecology.N
+
+	
+		return {
+		size = {x = 1,y = 4,z = 1},
+		data = {
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_trunk",prob = 254},
+			{param2 = 0,name = "lib_ecology:tree_fern_small_leaves_02",prob = 254}
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 127},
+			{ypos = 2,prob = 254},
+			{ypos = 3,prob = 254}
+		}
+	}
